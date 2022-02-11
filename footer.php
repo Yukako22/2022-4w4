1aebb4ecdd52a38e66527c029904412d1b2e4ecd
Alexandre <Yukako22@live.fr>
2022-02-11 17:55:33 -0500

    Modification du footer

--- footer.php
+++ footer.php
@@ -1,7 +1,16 @@
 <footer class="site__footer">
     <section>
-        <p>Mes plaisirs</p>
+        <p>Mes plaisirs de la vie</p>
         <?php wp_nav_menu(array("menu" => "simple", 
+
+                                "container"=> "nav",
+
+                                "container_class"=>"site__footer_nav")); ?>
+                                
+    </section>
+    <section>
+        <p>Ce que j'aime en multimédia</p>
+        <?php wp_nav_menu(array("menu" => "simple2", 
 
                                 "container"=> "nav",
 
